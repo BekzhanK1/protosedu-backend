01a3db00ac93f52134f6810a0a093ff71b20ae31
BekzhanK1 <bkimadieff@gmail.com>
2025-03-12 23:34:44 +0500

    fix: email sending

--- vunderkids/settings_config/development.py
+++ vunderkids/settings_config/development.py
@@ -4,5 +4,5 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
-FRONTEND_URL = "http://localhost:5173/"
-BACKEND_URL = "http://localhost:8000/"
+FRONTEND_URL = "https://protosedu.kz/"
+BACKEND_URL = "https://api.protosedu.kz/"
