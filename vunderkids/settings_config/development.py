4bd9d76293e302e96b6228faff83b20d7f6209f2
Protos <protosedu@protosedu.local>
2025-03-15 16:55:36 +0500

    fix

--- vunderkids/settings_config/development.py
+++ vunderkids/settings_config/development.py
@@ -7,10 +7,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 FRONTEND_URL = "https://protosedu.kz/"
 BACKEND_URL = "https://api.protosedu.kz/"
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://protosedu.kz",
-    "https://api.protosedu.kz",
-]
+CSRF_TRUSTED_ORIGINS = ["https://*", "http://*"]
+
 
 # Use secure cookies when serving over HTTPS.
 CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = False
