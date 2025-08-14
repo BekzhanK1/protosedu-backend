b6f2fcf0fd59efaa8d61ac6284066a161cef2f80
BekzhanK1 <bkimadieff@gmail.com>
2025-08-14 12:32:36 +0500

    fix test in django admin

--- modo/admin.py
+++ modo/admin.py
@@ -9,7 +9,29 @@ from .models import (
     TestCategory,
 )
 
-admin.site.register(Test)
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "test_type",
+        "get_language",
+        "category",
+        "order",
+    )
+    list_select_related = ("category",)  # optimizes queries
+    ordering = ("category__name", "order")  # first by category, then by order
+
+    def get_language(self, obj):
+        if obj.category:
+            return obj.category.language
+        return "-"
+
+    get_language.short_description = "Language"
+
+
 admin.site.register(Question)
 admin.site.register(Content)
 admin.site.register(AnswerOption)
